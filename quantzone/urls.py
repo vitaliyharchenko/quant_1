4e41a182cf7a553a7b770b696d6d1d9ae2d2f252
Vitaliy Kharchenko <harchenko.grape@gmail.com>
2016-09-07 14:53:04 +0500

    deploy changes

--- quantzone/urls.py
+++ quantzone/urls.py
@@ -30,4 +30,3 @@ urlpatterns += api.urls.urlpatterns
 urlpatterns += users.urls.urlpatterns
 urlpatterns += teaching.urls.urlpatterns
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
