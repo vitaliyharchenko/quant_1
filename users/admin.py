14b0fd57751980d9fb15ef17446c5301d42edb70
Vitaliy Kharchenko <harchenko.grape@gmail.com>
2016-09-24 16:04:36 +0500

    user admin tasks add

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from .models import User, UserActivation
+from teaching.models import LessonTask
+
+
+
+class LessonTaskInline(admin.TabularInline):
+    model = LessonTask
+    extra = 4
+
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [LessonTaskInline]
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(UserActivation)
 admin.site.unregister(Group)
