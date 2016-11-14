7e3c2ee5be981485a3aabdc23ed389c09f2df32c
vitaliyharchenko <harchenko.grape@gmail.com>
2016-11-14 18:33:29 +0500

    mail fixes

--- quantzone/settings.py
+++ quantzone/settings.py
@@ -121,7 +121,7 @@ VKONTAKTE_APP = {
 # EMail
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'info@sportcourts.ru'
-EMAIL_HOST_PASSWORD = 'courtssport2'
-EMAIL_SUBJECT_PREFIX = '[Quant] '
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_HOST_USER = 'info@quant.zone'
+EMAIL_HOST_PASSWORD = 'physicum123456'
+EMAIL_SUBJECT_PREFIX = '[QUANT] '
