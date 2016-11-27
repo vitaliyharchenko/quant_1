9ff6a8f3e5f112d53a5918f50abe37e85aaa20ff
Vitaliy Kharchenko <harchenko.grape@gmail.com>
2016-11-27 17:24:46 +0500

    Meta info styling

--- blocks/models.py
+++ blocks/models.py
@@ -36,52 +36,52 @@ class Block(models.Model):
 
 
 class TextBlock(Block):
+    title = models.CharField(max_length=200, unique=True)
+    body = MarkdownField()
+
     class Meta:
         verbose_name = 'текстовая статья'
         verbose_name_plural = 'текстовые статьи'
-
-    title = models.CharField(max_length=200, unique=True)
-    body = MarkdownField()
 
     def __str__(self):
         return self.title
 
 
 class ChoiceBlock(Block):
+    question_text = MarkdownField('Текст вопроса')
+    image = models.ImageField('Картинка', upload_to='choice_blocks/', null=True, blank=True)
+
     class Meta:
         verbose_name = 'тестовый вопрос'
         verbose_name_plural = 'тестовые вопросы'
-
-    question_text = MarkdownField('Текст вопроса')
-    image = models.ImageField('Картинка', upload_to='choice_blocks/', null=True, blank=True)
 
     def __str__(self):
         return self.question_text
 
 
 class ChoiceBlockOption(models.Model):
-    class Meta:
-        verbose_name = 'Вариант ответа на тестовый вопрос'
-        verbose_name_plural = 'Варианты ответа на тестовые вопросы'
-
     choice_block = models.ForeignKey(ChoiceBlock)
     option_text = models.CharField('Вариант ответа', max_length=600, blank=True)
     option_image = models.ImageField('Картинка', upload_to='choice_block_options/', null=True, blank=True)
     help_text = models.CharField('Подсказка', max_length=300, blank=True)
     is_true = models.BooleanField('Правильный?')
 
+    class Meta:
+        verbose_name = 'Вариант ответа на тестовый вопрос'
+        verbose_name_plural = 'Варианты ответа на тестовые вопросы'
+
     def __str__(self):
         return self.option_text
 
 
 class FloatBlock(Block):
-    class Meta:
-        verbose_name = 'задача с численным ответом'
-        verbose_name_plural = 'задачи с численным ответом'
-
     question_text = MarkdownField('Текст вопроса')
     image = models.ImageField('Картинка', upload_to='float_questions/', null=True, blank=True)
     answer = models.FloatField('Ответ')
+
+    class Meta:
+        verbose_name = 'задача с численным ответом'
+        verbose_name_plural = 'задачи с численным ответом'
 
     def __str__(self):
         return self.question_text
@@ -89,11 +89,10 @@ class FloatBlock(Block):
 
 # Включение блоков в урок
 class LessonBlockRelation(models.Model):
-    class Meta:
-        verbose_name = 'включение блока в урок'
-        verbose_name_plural = 'включения блоков в урок'
-        unique_together = ('lesson', 'block')
-
     lesson = models.ForeignKey('nodes.Lesson')
     block = models.ForeignKey(Block)
     order = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'включение блока в урок'
+        verbose_name_plural = 'включения блоков в урок'
