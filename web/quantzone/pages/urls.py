23f4d7e97f0d90b239a4c3fa1adb378563bd5621
Vitaliy <me@harchenko.pro>
2017-03-12 00:03:19 +0500

    katya changes

--- web/quantzone/pages/urls.py
+++ web/quantzone/pages/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index_view, name='index_view'),
 
+    url(r'^graph$', views.graph_view, name='graph_view'),
+
     url(r'^landing/(?P<landing_id>\d+)$', views.landing_view, name='landing_view'),
 ]
--- web/quantzone/pages/views.py
+++ web/quantzone/pages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
-from nodes.models import Subject
+from nodes.models import Subject, Node, NodeRelation
+import json
+import networkx as nx
+from networkx.readwrite import json_graph
 
 
 # Create your views here.
@@ -14,3 +17,17 @@ def landing_view(request, landing_id):
     subjects = Subject.objects.all().order_by('pk')
     tpl_str = u'landings/lp_{}.html'.format(landing_id)
     return render(request, tpl_str, {'subjects': subjects})
+
+
+def graph_view(request):
+    all_node_relations = NodeRelation.objects.all()
+
+    dg = nx.DiGraph()
+
+    for relation in all_node_relations:
+        dg.add_edge(str(relation.parent), str(relation.child))
+
+    data = json_graph.node_link_data(dg)
+    s = json.dumps(data)
+
+    return render(request, 'graph.html', {'s': s})
