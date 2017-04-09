ca1dac4406f2fc819289e1a86d9dcf29d0ccad4f
Vitaliy <me@harchenko.pro>
2017-04-09 09:44:58 +0500

    lms structure init

---
+++ web/quantzone/lms/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from users.models import User
+
+
+# Learning group
+class Group(models.Model):
+    title = models.CharField('Название группы', max_length=300)
+    owner = models.ForeignKey(User)
+    about = models.TextField()
+
+    class Meta:
+        verbose_name = 'Учебная группа'
+        verbose_name_plural = 'Учебные группы'
+
+    def __str__(self):
+        return self.title
+
+    @staticmethod
+    def get_absolute_url():
+        return None
--- web/quantzone/quantzone/settings.py
+++ web/quantzone/quantzone/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Custom apps
     'users',
+    'lms',
 
     # Third party
 
